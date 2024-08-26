be823a498e1c331acc20dcfd51cd3f5fac9089d3
philip-thomson <124644631+philip-thomson@users.noreply.github.com>
2024-08-26 23:59:12 +0000

    Aug 26, 2024, 11:59 PM

--- src/script.js
+++ src/script.js
@@ -1,42 +1,14 @@
 const area = document.querySelectorAll(".text-box");
-const button = document.querySelectorAll("button");
-let isSpeaking = false;
-let currentUtterance = null;
+const button = document.querySelector("button");
 
-button.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        textToSpeech(textBoxes[index], button);
-    });
-});
-
-const textToSpeech = (textBox, button) => {
+const textToSpeech = () => {
     const synth = window.speechSynthesis;
-    const text = textBox.innerText;
+    const text = area.innerText;
 
-
-    if (text.length > 50) {
-        if (synth.speaking) {
-            if (isSpeaking) {
-                button.innerText = "resume";
-                synth.pause();
-                isSpeaking = false;
-            } else {
-                button.innerText = "pause";
-                synth.resume();
-                isSpeaking = true;
-            }
-        } else {
-            currentUtterance = new SpeechSynthesisUtterance(text);
-            synth.speak(currentUtterance);
-            isSpeaking = true;
-            button.innerText = "pause";
-        }
-    } else {
-        if (!synth.speaking) {
-            currentUtterance = new SpeechSynthesisUtterance(text);
-            synth.speak(currentUtterance);
-            isSpeaking = true;
-            button.innerText = "pause";
-        }
+    if (!synth.speaking && text) {
+        const utternace = new SpeechSynthesisUtterance(text);
+        synth.speak(utternace);
     }
 };
+
+button.addEventListener("click", textToSpeech);
