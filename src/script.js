7554f195f9dd4a7188d5dc0a9c685b78b8ce5f23
philip-thomson <124644631+philip-thomson@users.noreply.github.com>
2024-08-26 01:14:03 +0000

    Aug 26, 2024, 1:14 AM

--- src/script.js
+++ src/script.js
@@ -1,13 +1,23 @@
 const area = document.querySelector("#text-box");
 const button = document.querySelector("button");
+let isSpeaking = True;
 
 const textToSpeech = () => {
     const synth = window.speechSynthesis;
     const text = area.innerText;
 
+
     if (!synth.speaking && text) {
         const utternace = new SpeechSynthesisUtterance(text);
         synth.speak(utternace);
+    }
+
+    if (text.length > 50) {
+        if (synth.speaking && isSpeaking) {
+            button.innerText = "paused";
+            synth.resume();
+            isSpeaking = False;
+        }
     }
 };
 
