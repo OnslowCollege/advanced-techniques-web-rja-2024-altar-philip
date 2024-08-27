9e477f88eb2adba205d5751a7f6aaac3494cf834
philip-thomson <124644631+philip-thomson@users.noreply.github.com>
2024-08-27 00:17:54 +0000

    Aug 27, 2024, 12:17 AM

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-const area = document.querySelector(".text-box");
+const area = document.querySelector("#text-box");
 const button = document.querySelector("button");
 
 const textToSpeech = () => {
     const synth = window.speechSynthesis;
-    const text = area.Value;
+    const text = area.getHTML();
 
     if (!synth.speaking && text) {
         const utternace = new SpeechSynthesisUtterance(text);
