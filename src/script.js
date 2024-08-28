6f97ac472e248087a37f59b5ac2449d14c50e5a0
philip-thomson <124644631+philip-thomson@users.noreply.github.com>
2024-08-28 22:13:40 +0000

    Aug 28, 2024, 10:13 PM

--- src/script.js
+++ src/script.js
@@ -1,19 +1,48 @@
 const area = document.querySelector("#text-box");
 const button = document.querySelector("#button_1");
-
-
 const button_2 = document.querySelector("#button_2");
 const area_v2 = document.querySelector("#text_box_v2");
 
-const textToSpeech = (paragraph) => {
-    const synth = window.speechSynthesis;
+let currentUtterance = null;
+let synth = window.speechSynthesis;
+let isSpeaking = false;
+let isPaused = false;
+
+const textToSpeech = (paragraph, button) => {
     const text = paragraph.textContent;
 
-    if (!synth.speaking && text) {
-        const utterance = new SpeechSynthesisUtterance(text);
-        synth.speak(utterance);
+    if (synth.speaking) {
+        if (isPaused) {
+            synth.resume();
+            button.textContent = "pause";
+        } else {
+            synth.pause();
+            button.textContent = "resume";
+            isPaused = true;
+            return;
+        }
+    }
+
+    if (text) {
+        if (currentUtterance) {
+            synth.cancel();
+        }
+
+        currentUtterance = new SpeechSynthesisUtterance(text)
+        currentUtterance.onend = () => {
+            isSpeaking = false;
+            isPaused = false;
+            button.textContent = "play"
+            currentUtterance = null;
+        };
+
+        synth.speaking(currentUtterance);
+        isSpeaking = true;
+        isPaused = false;
+        button.textContent = "pause";
+
     }
 };
 
-button.addEventListener("click", () => textToSpeech(area));
-button_2.addEventListener("click", () => textToSpeech(area_v2));
+button.addEventListener("click", () => textToSpeech(area, button));
+button_2.addEventListener("click", () => textToSpeech(area_v2, button_2));
