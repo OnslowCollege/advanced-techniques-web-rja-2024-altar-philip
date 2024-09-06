bb94fbdc9cdc73f86e4c37ba035cd344f954f048
philip-thomson <124644631+philip-thomson@users.noreply.github.com>
2024-09-06 00:51:16 +0000

    Sep 6, 2024, 12:51 AM

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
+
 const area = document.querySelector("#text-box");
 const button = document.querySelector("#button_1");
 const button_2 = document.querySelector("#button_2");
 const area_v2 = document.querySelector("#text_box_v2");
 
+// setting up all the changing state throughout the code within the textToSpeech constant
 let currentUtterance = null;
 let synth = window.speechSynthesis;
 let isSpeaking = false;
@@ -11,6 +13,7 @@ let isPaused = false;
 const textToSpeech = (paragraph, button) => {
     const text = paragraph.textContent;
 
+    // checking if the text is being read out and if isPaused is false setting the synth to being resumed and changing the buttons context to displaying a paused option if its not then its snyth to being pause and changes the display of the button to being resume and changes the state of isPaused
     if (synth.speaking) {
         if (isPaused) {
             synth.resume();
@@ -21,6 +24,7 @@ const textToSpeech = (paragraph, button) => {
             isPaused = true;
             return;
         }
+        // if the synth's is not speaking than it sets up a new speechsynthesis of text and changes the states of weither its in a changed state back to being false instead of true and changes the display text in the button back to its orginal state of text-to-speech
     } else {
         if (text) {
             if (currentUtterance) {
@@ -31,10 +35,10 @@ const textToSpeech = (paragraph, button) => {
             currentUtterance.onend = () => {
                 isSpeaking = false;
                 isPaused = false;
-                button.textContent = "play"
+                button.textContent = "Text-To-Speech"
                 currentUtterance = null;
             };
-
+            // setting currentUtterance to being spoken and changes the state of isSpeaking to true and the display of the text to being pause again
             synth.speak(currentUtterance);
             isSpeaking = true;
             isPaused = false;
@@ -43,5 +47,6 @@ const textToSpeech = (paragraph, button) => {
     }
 };
 
+// NEEEDS COMMENT SO GET IT DONE
 button.addEventListener("click", () => textToSpeech(area, button));
 button_2.addEventListener("click", () => textToSpeech(area_v2, button_2));
